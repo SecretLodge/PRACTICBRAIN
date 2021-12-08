99ed44a79b61bf73bf74d7824dddf0cbb3b4b2f2
SecretLodge <88157771+SecretLodge@users.noreply.github.com>
2021-12-08 07:20:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const createPath = (page) => path.resolve(__dirname, '', `${page}.pug`);
-const PORT = process.env.PORT || 22;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(express.static('assets'));
